b4667f8c5266e2af02584836cadb18b6ff836e01
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-12-28 00:12:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", () => {
+  console.log("Hello Hanh");
+  console.log("Hello Ho Van Hanh Root!!");
+});
+
+app.get("/health", (req, res) => {
+  console.log("API is supper power!!");
+  res.send("TEST HEALTH")
+});
+
+app.listen(3000, () => {
+  console.log("App is running on localhost://3000");
+});
